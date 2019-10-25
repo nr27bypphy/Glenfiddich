7b3eb0111961e6159dbfc779c577ee40adc5d704
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-10-26 07:42:36 +0900

    ✨ create dashboard # index page

---
+++ task_management/client/bundles/Dashboard/components/DashboardPage.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function DashboardPage() {
+  return (
+    <>
+      <div>hello this is dashbaord</div>
+    </>
+  );
+}
+
+export default _ => <DashboardPage />;
