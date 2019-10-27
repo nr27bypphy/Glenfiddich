adf9f834e394bca7fe11c378fc0f3b0feaeb3089
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-10-27 10:27:05 +0900

    session view で flash,flash_contentを使えるようにした

--- task_management/client/packs/session-bundle.js
+++ task_management/client/packs/session-bundle.js
@@ -1,7 +1,11 @@
 import ReactOnRails from "react-on-rails";
 
 import LoginContents from "../bundles/Session/components/LoginContents";
+import FlashContent from "../bundles/Shared/components/FlashContent";
+import Flash from "../bundles/Shared/components/Flash";
 
 ReactOnRails.register({
-  LoginContents
+  LoginContents,
+  FlashContent,
+  Flash
 });
