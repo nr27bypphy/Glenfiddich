00830445e1e99af98052b08eb86e445c45ae0934
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-11-09 09:48:13 +0900

    DashboardPageにDashboardContainerを追加

--- task_management/client/bundles/Dashboard/components/DashboardPage.jsx
+++ task_management/client/bundles/Dashboard/components/DashboardPage.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import DashboardHeader from "./DashbboardHeader";
+import DashboardContainer from "./DashboardContainer";
 
 function DashboardPage() {
   return (
     <>
       <DashboardHeader />
+      <DashboardContainer />
     </>
   );
 }
