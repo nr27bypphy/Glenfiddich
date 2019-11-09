ba8ce64db6db11867c50a9d6a5d33b9108796f0b
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-11-09 14:20:53 +0900

    ✅ Add spec for user model

---
+++ task_management/spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "Validaiton" do
+    subject { user }
+    let(:user) { create(:user) }
+
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:mail) }
+    it { is_expected.to validate_uniqueness_of(:mail) }
+  end
+end
