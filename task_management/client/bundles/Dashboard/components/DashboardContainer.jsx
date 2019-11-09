f032416a8c2a502f21d181f3fc2d753dadf38d52
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-11-09 18:00:07 +0900

    ♻️  プロジェクト追加モーダルをAddProjectModalに切り出した

--- task_management/client/bundles/Dashboard/components/DashboardContainer.jsx
+++ task_management/client/bundles/Dashboard/components/DashboardContainer.jsx
@@ -14,14 +14,7 @@ import { ProjectTableTr } from "./ProjectTableTr";
 import { MemberTableTr } from "./MemberTableTr";
 import DescriptionIcon from "@material-ui/icons/Description";
 import PeopleIcon from "@material-ui/icons/People";
-
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
+import { AddProjectModal } from "./AddProjectModal";
 
 const useStyles = makeStyles(theme => ({
   projectPaper: {
@@ -94,35 +87,7 @@ export const DashboardContainer = props => {
         </Content>
       </BackgroundThema>
       {/* プロジェクト追加モーダル */}
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Subscribe
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <AddProjectModal open={open} handleClose={() => handleClose()} />
     </>
   );
 };
