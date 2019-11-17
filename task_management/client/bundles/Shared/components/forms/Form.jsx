ec348cbafdf81e47e80f61896970eb3c07f4f378
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-11-17 23:39:26 +0900

    ✨ create Form component

---
+++ task_management/client/bundles/Shared/components/forms/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactOnRails from "react-on-rails";
+
+export const Form = props => {
+  const csrfToken = ReactOnRails.authenticityToken();
+
+  return (
+    <form action={props.action} method={props.method}>
+      {props.children}
+      {/* CSRFトークン対策 */}
+      <input type="hidden" name="authenticity_token" value={csrfToken} />
+    </form>
+  );
+};
