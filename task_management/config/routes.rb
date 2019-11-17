488b030258b792070aec01d0b77c8a5438a0cf30
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-11-18 00:37:27 +0900

    usersのindex アクションのroutingを追加

--- task_management/config/routes.rb
+++ task_management/config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  resources :users, only: %i(index)
+
   root to: "dashboards#index"
 end
