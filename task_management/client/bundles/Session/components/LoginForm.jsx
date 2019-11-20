8d89ef4b5e1058f8c83791485af8a9eee1dd0951
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-11-21 01:57:06 +0900

    👍 ログインページを刷新した
    ほぼmaterial uiのテーマをパクってきただけ

--- task_management/client/bundles/Session/components/LoginForm.jsx
+++ task_management/client/bundles/Session/components/LoginForm.jsx
@@ -1,45 +1,126 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { SessionTextField } from "./SessionTextField";
-import { SubmitButton } from "../../Shared/components/forms/SubmitButton";
-import { LinkText } from "../../Shared/components/forms/LinkText";
-import { FormHeader } from "../../Shared/components/forms/FormHeader";
-import { FormElementWrapper } from "../../Shared/components/forms/FormElementWrapper";
-import { FormBodyWrapper } from "../../Shared/components/forms/FormBodyWrapper";
-import { FormCard } from "../../Shared/components/forms/FormCard";
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
 import { Form } from "../../Shared/components/forms/Form";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-  formContentWrapper: {
-    width: "260px",
-    margin: "0 auto"
+  root: {
+    height: "100vh"
+  },
+  image: {
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor: theme.palette.grey[50],
+    backgroundSize: "cover",
+    backgroundPosition: "center"
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
   }
 }));
 
-export const LoginForm = props => {
+export const LoginForm = _ => {
   const classes = useStyles();
+  const [checkedValue, setCheckedValue] = useState(false);
+  const handleChange = () => e => {
+    setCheckedValue(e.target.checked);
+  };
 
   return (
-    <FormCard height="420px">
-      <FormHeader>ログインフォーム</FormHeader>
-      <Form action="/login" method="post">
-        <FormBodyWrapper>
-          <FormElementWrapper>
-            <SessionTextField
-              label="メールアドレス"
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Form
+            className={classes.form}
+            action="/login"
+            method="post"
+            noValidate
+          >
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
               name="session[mail]"
-              hello="hoge"
+              autoComplete="email"
+              autoFocus
             />
-          </FormElementWrapper>
-          <FormElementWrapper>
-            <SessionTextField label="パスワード" name="session[password]" />
-          </FormElementWrapper>
-          <div>
-            <SubmitButton />
-          </div>
-          <LinkText href="/users/new">新規登録はこちらから</LinkText>
-        </FormBodyWrapper>
-      </Form>
-    </FormCard>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="session[password]"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  color="primary"
+                  name="session[remember_me]"
+                  onChange={handleChange()}
+                  value={checkedValue}
+                />
+              }
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              {/* @todo パスワードリセット機能をここに追加する */}
+              <Grid item>
+                <Link href="users/new" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Form>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
