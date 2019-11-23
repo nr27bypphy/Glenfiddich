7d86e611e8cb7a806040dede33166e9a50256894
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-11-23 11:11:30 +0900

    fix Interger -> Int

--- task_management/app/graphql/types/task_type.rb
+++ task_management/app/graphql/types/task_type.rb
@@ -2,7 +2,7 @@ module Types
   class TaskType < Types::BaseObject
     field :id, ID, null: false
     field :title, String, null: false
-    field :user_id, Integer, null: false
+    field :user_id, Int, null: false
     field :description, String, null: false
   end
 end
