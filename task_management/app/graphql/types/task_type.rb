bb640989f8fa47319fee087e1a0eaefbf91632d8
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-11-23 10:27:43 +0900

    ✨ task の queryを作成し、追加した

---
+++ task_management/app/graphql/types/task_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class TaskType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :user_id, Integer, null: false
+    field :description, String, null: false
+  end
+end
