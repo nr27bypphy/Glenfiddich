ec97ce2f4792a03d0eacfbff98f2b0a45f4110b1
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-11-23 10:56:05 +0900

    task 追加の mutationを実装

--- task_management/app/graphql/mutations/create_task.rb
+++ task_management/app/graphql/mutations/create_task.rb
@@ -1,5 +1,18 @@
 module Mutations
   class CreateTask < GraphQL::Schema::RelayClassicMutation
+    argument :title, String, required: true
+    argument :user_id, Integer, required: true
+    argument :description, String, required: true
+
+    field :task, Types::TaskType, null: false
+
+    def resolve(title:, user_id:, description:)
+      task = Task.new(title: title, user_id: user_id, description: description)
+      task.save!
+
+      { task: task }
+    end
+
     # TODO: define return fields
     # field :post, Types::PostType, null: false
 
