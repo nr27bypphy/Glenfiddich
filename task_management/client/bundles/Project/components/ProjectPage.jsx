f6fa39b6cc6174da910b19811f90fb3a3e5e005e
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-12-04 00:35:32 +0900

    wip

--- task_management/client/bundles/Project/components/ProjectPage.jsx
+++ task_management/client/bundles/Project/components/ProjectPage.jsx
@@ -2,10 +2,92 @@ import React from "react";
 import { Header } from "../../Shared/components/Header";
 import { ApolloProviderWrapper } from "../../Shared/components/ApolloProviderWrapper";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1
+  },
+  list: {
+    backgroundColor: "#313985"
+  },
+  listText: {
+    color: "#fff"
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  },
+  toolbar: theme.mixins.toolbar
+}));
+
 function ProjectPage(props) {
+  const classes = useStyles();
+
   return (
     <ApolloProviderWrapper>
-      <Header user={props.user} />
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Header />
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper
+          }}
+        >
+          <div className={classes.toolbar} />
+          <List className={classes.list}>
+            <ListItem>
+              <Typography
+                variant="h6"
+                align="left"
+                className={classes.listText}
+              >
+                Project
+              </Typography>
+            </ListItem>
+            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon style={{ color: "white" }}>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} className={classes.listText} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Typography paragraph></Typography>
+          <Typography paragraph></Typography>
+        </main>
+      </div>
     </ApolloProviderWrapper>
   );
 }
