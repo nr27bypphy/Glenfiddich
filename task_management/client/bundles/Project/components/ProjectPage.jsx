92f75a9403023ffbe239a713b944a2f6ee8221f6
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-12-03 22:29:14 +0900

    headerを追加

--- task_management/client/bundles/Project/components/ProjectPage.jsx
+++ task_management/client/bundles/Project/components/ProjectPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Header } from "../../Shared/components/Header";
+import { ApolloProviderWrapper } from "../../Shared/components/ApolloProviderWrapper";
 
 function ProjectPage(props) {
-  return <div>hello world</div>;
+  return (
+    <ApolloProviderWrapper>
+      <Header user={props.user} />
+    </ApolloProviderWrapper>
+  );
 }
 
 export default props => <ProjectPage {...props} />;
