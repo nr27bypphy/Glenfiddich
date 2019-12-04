87171c44801aef8d479f15074ef5c476aedb57c6
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-12-05 00:46:47 +0900

    🚿 不要なqueryとmutationを削除

--- task_management/app/graphql/types/mutation_type.rb
+++ task_management/app/graphql/types/mutation_type.rb
@@ -1,11 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
     field :createTask, mutation: Mutations::CreateTask
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
   end
 end
