a60de0828ff0b6e61415b432b0f5e6013b3d8198
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-12-05 22:59:59 +0900

    ✨ 20件のprojectとtaskのrelationをfront側に渡すようにした

--- task_management/app/controllers/projects_controller.rb
+++ task_management/app/controllers/projects_controller.rb
@@ -1,5 +1,15 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = current_user.projects
+    @projects = current_user
+                  .projects
+                  .last(20)
+                  .to_json(
+                    only: [:title, :description, :deadline],
+                    include: {
+                      tasks: {
+                        only: [:title, :description, :deadline]
+                      }
+                    }
+                  )
   end
 end
