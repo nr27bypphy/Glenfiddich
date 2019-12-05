6737bc12abf7a6041a5a96d116544334cbd39318
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-12-05 23:14:58 +0900

    🚿 不要になったcomponentのimportを削除する

--- task_management/client/bundles/Project/components/ProjectPage.jsx
+++ task_management/client/bundles/Project/components/ProjectPage.jsx
@@ -8,12 +8,9 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
 import WorkIcon from "@material-ui/icons/Work";
 import { TaskTable } from "./TaskTable";
 
