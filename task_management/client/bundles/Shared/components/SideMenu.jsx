2bcd096f8dc712ff35209b408136d3020c841647
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-12-06 21:08:27 +0900

    👍 サイドメニューを作成

--- task_management/client/bundles/Shared/components/SideMenu.jsx
+++ task_management/client/bundles/Shared/components/SideMenu.jsx
@@ -1,30 +1,34 @@
-import React, { useState }  from 'react';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   list: {
-    width: 250,
-  },
+    width: 250
+  }
 }));
+
+const sideMenuContent = [
+  { text: "ダッシュボード", path: "/" },
+  { text: "Hoge", path: "/" },
+  { text: "ユーザー詳細", path: "/projects" }
+];
+
 export const SideMenu = side => {
   const classes = useStyles();
-  return(
-    <div
-      className={classes.list}
-      role="presentation"
-    >
-    <List>
-    {['DashBoard', 'User Management', 'Project managemen'].map((text) => (
-      <ListItem button key={text}>
-      <ListItemText primary={text} />
-      </ListItem>
-    ))}
-    </List>
-    <Divider />
+  return (
+    <div className={classes.list} role="presentation">
+      <List>
+        {sideMenuContent.map((item, index) => (
+          <ListItem button component="a" href={item.path} key={index}>
+            <ListItemText primary={item.text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
     </div>
-  )
+  );
 };
