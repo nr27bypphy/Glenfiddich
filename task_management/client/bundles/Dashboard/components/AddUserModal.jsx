f9ecbd6699f00dce0f83395157effdb6af3f5708
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-12-07 14:49:48 +0900

    ✨ User登録のmodalを修正

--- task_management/client/bundles/Dashboard/components/AddUserModal.jsx
+++ task_management/client/bundles/Dashboard/components/AddUserModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
@@ -7,14 +7,28 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  }
+}));
 
 export const AddUserModal = props => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [userName, setUserName] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const classes = useStyles();
 
   const handleAddProject = () => {
-    props.addNewTasks(title, description);
     props.handleClose();
   };
 
@@ -26,45 +40,67 @@ export const AddUserModal = props => {
     >
       <DialogTitle id="form-dialog-title">メンバーの追加</DialogTitle>
       <DialogContent>
-        <DialogContentText>
-          メンバーの追加が可能です！
-          <br />
-          タイトルと詳細、担当者を入力しましょう✨
-        </DialogContentText>
+        <DialogContentText>メンバーの追加が可能です！</DialogContentText>
         <TextField
           autoFocus
           margin="dense"
           id="title"
-          label="タイトル"
-          type="text"
-          value={title}
+          label="メンバー名"
+          type="name"
+          value={name}
           onChange={e => {
-            setTitle(e.target.value);
+            setName(e.target.value);
           }}
           fullWidth
         />
         <TextField
           autoFocus
           margin="dense"
-          id="description"
-          label="詳細"
+          id="mail"
+          label="メールアドレス"
           type="text"
-          value={description}
+          value={email}
           onChange={e => {
-            setDescription(e.target.value);
+            setEmail(e.target.value);
+          }}
+          fullWidth
+        />
+        <FormControl className={classes.formControl}>
+          <InputLabel>権限</InputLabel>
+          <Select
+            value={role}
+            onChange={e => {
+              setRole(e.target.value);
+            }}
+          >
+            <MenuItem value={0}>オーナー</MenuItem>
+            <MenuItem value={1}>管理者</MenuItem>
+            <MenuItem value={2}>一般</MenuItem>
+            <MenuItem value={3}>ゲスト</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="password"
+          label="パスワード"
+          type="password"
+          value={password}
+          onChange={e => {
+            setPassword(e.target.value);
           }}
           fullWidth
         />
         <TextField
           autoFocus
           margin="dense"
-          id="user_name"
-          label="担当者名"
-          value={userName}
+          id="password-confirmation"
+          label="パスワード確認"
+          type="password"
+          value={passwordConfirmation}
           onChange={e => {
-            setUserName(e.target.value);
+            setPasswordConfirmation(e.target.value);
           }}
-          type="text"
           fullWidth
         />
       </DialogContent>
