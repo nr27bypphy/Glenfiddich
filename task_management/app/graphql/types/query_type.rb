8ad7b751936a060bfa096464d284e408e1171041
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-12-09 23:55:29 +0900

    format修正

--- task_management/app/graphql/types/query_type.rb
+++ task_management/app/graphql/types/query_type.rb
@@ -1,7 +1,10 @@
 module Types
   class QueryType < Types::BaseObject
-    field :tasks, Types::TaskType.connection_type, null: false, resolve: -> (_object, _args, _context) do
-      Task.all
-    end
+    field :tasks,
+          Types::TaskType.connection_type,
+          null: false,
+          resolve: ->(_object, _args, _context) do
+            Task.all
+          end
   end
 end
