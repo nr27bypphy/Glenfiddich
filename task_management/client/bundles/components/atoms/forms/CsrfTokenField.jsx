77b83f792794b94dd4ca0a603a8befb9bf55d4a6
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-12-15 17:58:24 +0900

    ✨ CsrfTokenFieldを作成

---
+++ task_management/client/bundles/components/atoms/forms/CsrfTokenField.jsx
@@ -0,0 +1,8 @@
+import ReactOnRails from "react-on-rails";
+
+export const CsrfTokenField = _ => {
+  const csrfToken = ReactOnRails.authenticityToken();
+
+  // CSRFトークン対策
+  return <input type="hidden" name="authenticity_token" value={csrfToken} />;
+};
