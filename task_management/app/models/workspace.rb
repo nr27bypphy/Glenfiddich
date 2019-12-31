1e38f72d35a915ee88a7093c28bbfce9f91c1c33
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-12-31 22:01:14 +0900

    case_sensitiveのオプションを追加

--- task_management/app/models/workspace.rb
+++ task_management/app/models/workspace.rb
@@ -2,5 +2,5 @@ class Workspace < ApplicationRecord
 
   has_many :workspace_members
 
-  validates :name, null: false, uniqueness: true
+  validates :name, null: false, uniqueness: { case_sensitive: false }
 end
