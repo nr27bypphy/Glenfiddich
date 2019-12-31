168ea94ecdeae76555c5d9cf49d9f16d29f08cea
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2019-12-31 22:00:48 +0900

    🐛 presence制約が間違っていた。。。。

--- task_management/app/models/workspace_member.rb
+++ task_management/app/models/workspace_member.rb
@@ -5,7 +5,7 @@ class WorkspaceMember < ApplicationRecord
 
   enum role: { owner: 0, admin: 1, normal: 2, guest: 3 }
 
-  validates :workspace, presence: false
+  validates :workspace, presence: true
   validates :user, presence: true
   validates :workspace_id, uniqueness: { scope: :user_id }
   validates :role, presence: true
