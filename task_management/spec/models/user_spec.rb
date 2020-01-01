f4eee26a4f706fb541ea48e996e1845964705b6d
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-01-01 23:31:13 +0900

    🐛 fix spec mail -> email

--- task_management/spec/models/user_spec.rb
+++ task_management/spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
     let(:user) { create(:user) }
 
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:mail) }
+    it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     it { is_expected.to validate_presence_of(:role) }
     it { is_expected.to define_enum_for(:role).with_values([:owner, :admin, :normal, :guest]) }
