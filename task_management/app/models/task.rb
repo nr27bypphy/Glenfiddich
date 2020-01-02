382b3f7b18eda7e4cf858b8e1888e98a2bd1ca86
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-01-02 11:06:38 +0900

    task modelのassociationとspecを修正

--- task_management/app/models/task.rb
+++ task_management/app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
 
   belongs_to :project
+  belongs_to :workspace_member
 
   validates :title, presence: true
   validates :project, presence: true
--- task_management/spec/models/task_spec.rb
+++ task_management/spec/models/task_spec.rb
@@ -3,5 +3,11 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe "Validation" do
     it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:project) }
+  end
+
+  describe 'Association' do
+    it { is_expected.to belong_to(:project) }
+    it { is_expected.to belong_to(:workspace_member) }
   end
 end
