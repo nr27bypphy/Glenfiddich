dd4aeac232c63e99dafe3e201e5117ddf4809420
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-02-02 23:32:03 +0900

    DashboardPageをリファクタ

--- task_management/client/bundles/containers/pages/DashboardPage.jsx
+++ task_management/client/bundles/containers/pages/DashboardPage.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { DashboardContainer } from "../templates/DashboardContainer";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { client } from "../../../lib/ApolloClient/client"
 import { Header } from "../../components/organisms/Header";
-import { ApolloProviderWrapper } from "../../components/providers/ApolloProviderWrapper";
 
 const DashboardPage = props => {
   return (
-    <ApolloProviderWrapper>
+    <ApolloProvider client={client}>
       <Header user={props.user} />
       <DashboardContainer
         tasks={props.tasks}
         users={props.users}
         workspaceId={props.workspaceId}
       />
-    </ApolloProviderWrapper>
+    </ApolloProvider>
   );
 };
 
