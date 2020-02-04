ad325b21134ffe2d16c887daf3e3c86fe2014309
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-02-05 00:31:43 +0900

    projectを追加するmutationを作成

---
+++ task_management/app/graphql/mutations/create_project.rb
@@ -0,0 +1,22 @@
+module Mutations
+  class CreateProject < GraphQL::Schema::RelayClassicMutation
+    argument :title, String, required: true
+    argument :description, String, required: true
+    argument :workspace_member_id, Integer, required: false
+
+    field :project, Types::ProjectType, null: false
+
+    def resolve(title:, description:, workspace_member_id:)
+      current_workspace = context[:current_workspace]
+      current_workspace
+        .projects
+        .create!(
+          title: title,
+          description: description,
+          workspace_member_id: workspace_member_id
+        )
+
+      { project: current_workspace }
+    end
+  end
+end
--- task_management/app/graphql/types/mutation_type.rb
+++ task_management/app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
     field :createUser, mutation: Mutations::CreateUser
     field :destroyUser, mutation: Mutations::DestroyUser
     field :invitationWorkspaceMember, mutation: Mutations::InvitationWorkspaceMember
+    field :createProject, mutation: Mutations::CreateProject
   end
 end
