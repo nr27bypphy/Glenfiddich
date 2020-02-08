7aa95b0f7c2374be2785c5828f4e16fd65c382ef
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-02-08 16:58:52 +0900

    fix typo

--- task_management/app/graphql/types/project_type.rb
+++ task_management/app/graphql/types/project_type.rb
@@ -4,6 +4,6 @@ module Types
     field :title, String, null: false, description: 'プロジェクトのタイトル'
     field :description, String, null: true, description: 'プロジェクトの概要'
     field :workspace, Types::WorkspaceType, null: false, description: '紐づいているワークスペース'
-    field :workspaceMember, Types::WorkspaceMemberType, null: true, desciption: '担当のワークスペースメンバー'
+    field :workspaceMember, Types::WorkspaceMemberType, null: true, description: '担当のワークスペースメンバー'
   end
 end
