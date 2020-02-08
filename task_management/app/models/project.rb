7fcd598f74eed4213637de2272d6585ef79dc301
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-02-08 16:59:10 +0900

    projectでworkspace_memberは初期は必須を解除

--- task_management/app/models/project.rb
+++ task_management/app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
 
   has_many :tasks
   belongs_to :workspace
-  belongs_to :workspace_member
+  belongs_to :workspace_member, required: false
 
   validates :title, presence: true
   validates :workspace, presence: true
