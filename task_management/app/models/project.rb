944ebf5486a234064e4ad5f74c6ceb8a0990e466
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-02-11 19:26:55 +0900

    🎉 projectにバリデーションを追加

--- task_management/app/models/project.rb
+++ task_management/app/models/project.rb
@@ -6,4 +6,18 @@ class Project < ApplicationRecord
 
   validates :title, presence: true
   validates :workspace, presence: true
+  validate :validate_workspace_member_belongs_to_workspace
+
+  private
+
+  # 設定した担当者が workspace に所属するメンバーかをチェックする
+  def validate_workspace_member_belongs_to_workspace
+    # workspace_member が nil の時はチェックの対象外なので return
+    return if workspace_member.nil?
+
+    member = workspace.workspace_members.find_by(id: workspace_member.id)
+    return if member.present?
+
+    errors.add(:base, '設定した担当者が不正です')
+  end
 end
