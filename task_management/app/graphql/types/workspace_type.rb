19284e3547cafe465c5593c22c24181cdcb88f1c
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-02-11 19:33:23 +0900

    workspace_typeのdescriptionを追加

--- task_management/app/graphql/types/workspace_type.rb
+++ task_management/app/graphql/types/workspace_type.rb
@@ -1,6 +1,6 @@
 module Types
   class WorkspaceType < Types::BaseObject
-    field :id, ID, null: false
-    field :name, String, null: false
+    field :id, ID, null: false, description: 'ワークスペースのid'
+    field :name, String, null: false, description: 'ワークスペース名'
   end
 end
