7e45742578a0e51f7344538830d2704b9abe641d
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-02-11 18:32:01 +0900

    📝 descriptionを追記した

--- task_management/app/graphql/types/mutation_type.rb
+++ task_management/app/graphql/types/mutation_type.rb
@@ -1,9 +1,9 @@
 module Types
   class MutationType < Types::BaseObject
-    field :createTask, mutation: Mutations::CreateTask
-    field :createUser, mutation: Mutations::CreateUser
-    field :destroyUser, mutation: Mutations::DestroyUser
-    field :invitationWorkspaceMember, mutation: Mutations::InvitationWorkspaceMember
-    field :createProject, mutation: Mutations::CreateProject
+    field :createTask, mutation: Mutations::CreateTask, description: 'タスクを新規作成する'
+    field :createUser, mutation: Mutations::CreateUser, description: 'userを作成する'
+    field :destroyUser, mutation: Mutations::DestroyUser, description: 'userを削除する'
+    field :invitationWorkspaceMember, mutation: Mutations::InvitationWorkspaceMember, description: 'ワークスペースメンバーを招待する'
+    field :createProject, mutation: Mutations::CreateProject, description: 'プロジェクトを新規作成する'
   end
 end
