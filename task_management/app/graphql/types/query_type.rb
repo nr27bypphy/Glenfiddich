cfffe51e99674f785bb05338e4f3df1654cd460a
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-02-13 23:20:56 +0900

    projects の typeを追加

--- task_management/app/graphql/types/query_type.rb
+++ task_management/app/graphql/types/query_type.rb
@@ -13,10 +13,16 @@ module Types
             User.all
           end
     field :workspace_members, [Types::WorkspaceMemberType], null: true, description: '現在使用しているワークスペースの全メンバー'
+    field :projects,  [Types::ProjectType], null: true, description: 'ログイン中のワークスペースに紐づくプロジェクト'
     
     def workspace_members
       workspace = context[:current_workspace]
       workspace.workspace_members
+    end
+
+    def projects
+      workspace = context[:current_workspace]
+      workspace.projects
     end
   end
 end
