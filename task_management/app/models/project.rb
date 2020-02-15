e0ac37f575b02168487916291181aa377fae52b6
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-02-15 10:10:16 +0900

    ♻️  validate_workspace_member_belongs_to_workspaceのチェック方法が煩雑だったので修正

--- task_management/app/models/project.rb
+++ task_management/app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ApplicationRecord
 
   has_many :tasks
   belongs_to :workspace
+  # 主担当者
   belongs_to :workspace_member, required: false
 
   validates :title, presence: true
@@ -10,13 +11,13 @@ class Project < ApplicationRecord
 
   private
 
-  # 設定した担当者が workspace に所属するメンバーかをチェックする
+  # 主担当者 が紐づいている workspace と プロジェクトが紐づいている workspace が一致しているか
   def validate_workspace_member_belongs_to_workspace
     # workspace_member が nil の時はチェックの対象外なので return
     return if workspace_member.nil?
 
-    member = workspace.workspace_members.find_by(id: workspace_member.id)
-    return if member.present?
+    # 紐づいている workspace が一致しているなら return
+    return if workspace_member.workspace == workspace
 
     errors.add(:base, '設定した担当者が不正です')
   end
