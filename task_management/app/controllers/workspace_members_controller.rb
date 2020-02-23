64af7bd2e09be6d6c8c06c001e1469b08b32580e
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-02-24 02:06:33 +0900

    workspace_members_controllerに詳細ページ用のshowを追加した

--- task_management/app/controllers/workspace_members_controller.rb
+++ task_management/app/controllers/workspace_members_controller.rb
@@ -3,4 +3,25 @@
 class WorkspaceMembersController < ApplicationController
   def index
   end
+
+  def show
+    @projects = current_workspace
+                  .projects
+                  .last(20)
+                  .to_json(
+                    only: [:title, :description, :deadline],
+                    include: {
+                      tasks: {
+                        only: [:title, :description, :deadline]
+                      }
+                    }
+                  )
+    @workspace_member = current_workspace
+                          .workspace_members
+                          .find(params[:id])
+
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "該当のユーザーが存在しません"
+    redirect_back(fallback_location: root_path)
+  end
 end
