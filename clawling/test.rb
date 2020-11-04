b099fe8c7dfc220d9403d0c234214eb649960b3e
戸田成彦 <n_toda@todanaruhikonoMacBook-Pro.local>
2020-11-04 22:57:38 +0900

    enable to get all element in town page

--- clawling/test.rb
+++ clawling/test.rb
@@ -14,25 +14,66 @@ wait = Selenium::WebDriver::Wait.new(timeout: wait_time)
 driver.get('https://www.mapion.co.jp/phonebook/')
 
 begin
-  head = "会社名,TEL\n"
-  category = driver.find_element(:css, 'body > div.wrapper > div.container.clearfix > div.article > section:nth-child(6) > div > section.phonebook-cat.phonebook-cat-M01 > ul > li:nth-child(1) > a')
-  category.click
-  
-  prefecture = driver.find_element(:css, 'body > div.wrapper > div.container.clearfix > div.article > section > table > tbody > tr:nth-child(1) > td > a:nth-child(1)')
-  prefecture.click
-  
-  cities = driver.find_elements(:css, 'body > div.wrapper > div.container.clearfix > div.article > section.section.type-a > ul > li')
-  city_num = cities.length
-  
+  head = "会社名,TEL,都道府県,市区町村,カテゴリ,ジャンル\n"
+  genres = driver.find_elements(:css, 'body > div.wrapper > div.container.clearfix > div.article > section:nth-child(6) > div > section.phonebook-cat')
+  genre_num = genres.length
+
   csv = CSV.generate(head, headers: true) do |csv|
-    1.upto(city_num) do |i|
-      city = driver.find_element(:css, "body > div.wrapper > div.container.clearfix > div.article > section.section.type-a > ul > li:nth-child(#{i}) > a")
-      city.click
-      store_name = driver.find_element(:css, 'body > div.wrapper > div.container.clearfix > div.article > section.section.type-a > table > tbody > tr > th > a')
-      tel = driver.find_element(:css, 'body > div.wrapper > div.container.clearfix > div.article > section.section.type-a > table > tbody > tr > td:nth-child(3)')
-      csv.add_row([store_name.text, tel.text])
-      driver.navigate.back
-      sleep(1)
+
+    1.upto(genre_num) do |l|
+      genre = driver.find_element(:css, "body > div.wrapper > div.container.clearfix > div.article > section:nth-child(6) > div > section.phonebook-cat.phonebook-cat-M#{format('%02d', l)} > h2 > a")
+      p genre_name = genre.text
+      
+      p format('%02d', l)
+      categories = driver.find_elements(:css, "body > div.wrapper > div.container.clearfix > div.article > section:nth-child(6) > div > section.phonebook-cat.phonebook-cat-M#{format('%02d', l)} > ul > li")
+      category_num = categories.length
+  
+      1.upto(category_num) do |k|
+        category = driver.find_element(:css, "body > div.wrapper > div.container.clearfix > div.article > section:nth-child(6) > div > section.phonebook-cat.phonebook-cat-M#{format('%02d', l)} > ul > li:nth-child(#{k}) > a")
+        p category_name = category.text
+        category.click
+        sleep(1)
+    
+        # prefectures = driver.find_elements(:css, "body > div.wrapper > div.container.clearfix > div.article > section > table > tbody > tr:nth-child(2) > td > a") 
+        # prefecture_num = prefectures.length
+        prefecture_num = 4
+        
+        1.upto(prefecture_num) do |j|
+          prefecture = driver.find_element(:css, "body > div.wrapper > div.container.clearfix > div.article > section > table > tbody > tr:nth-child(2) > td > a:nth-child(#{j})")
+          p prefecture_name = prefecture.text
+          prefecture.click
+          sleep(1)
+    
+          cities = driver.find_elements(:css, 'body > div.wrapper > div.container.clearfix > div.article > section.section.type-a > ul > li')
+          city_num = cities.length
+        
+          1.upto(city_num) do |i|
+            city = driver.find_element(:css, "body > div.wrapper > div.container.clearfix > div.article > section.section.type-a > ul > li:nth-child(#{i}) > a")
+            p city_name = city.text
+            city.click
+            sleep(1)
+    
+            stores = driver.find_elements(:css, 'body > div.wrapper > div.container.clearfix > div.article > section.section.type-a > table > tbody > tr') 
+            store_num = stores.length
+
+            1.upto(store_num) do |h|
+              store = driver.find_element(:css, "body > div.wrapper > div.container.clearfix > div.article > section.section.type-a > table > tbody > tr:nth-child(#{h}) > th > a")
+              store_name = store.text
+              tel = driver.find_element(:css, "body > div.wrapper > div.container.clearfix > div.article > section.section.type-a > table > tbody > tr:nth-child(#{h}) > td:nth-child(3)")
+              tel_number = tel.text
+              csv.add_row([store_name, tel_number, prefecture_name, city_name, category_name, genre_name])
+              break if h > 1
+            end
+            driver.navigate.back
+            break if i > 1
+          end
+          driver.navigate.back
+          break if j > 1
+        end
+        driver.navigate.back
+        break if k > 1
+      end
+      break if l > 1
     end
   end
   
